59205b9a9115b5511d39101843805b34ea3ee709
Larry <66905431+larry-oj@users.noreply.github.com>
2022-12-15 12:46:32 +0200

    book controller

---
+++ backend/React-AspNet-Identity/Controllers/BooksController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using React_AspNet_Identity.Data;
+using React_AspNet_Identity.Data.Models;
+using React_AspNet_Identity.Models;
+
+namespace React_AspNet_Identity.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BooksController : ControllerBase
+{
+    private readonly BackendContext _context;
+    
+    public BooksController(BackendContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet]
+    public IActionResult GetBooks()
+    {
+        return Ok(_context.Books.Include(b => b.Author).ToList().Select(x => new Book
+        {
+            Title = x.Title,
+            AuthorUsername = x.Author.UserName
+        }));
+    }
+    
+    [HttpGet]
+    [Route("{title}")]
+    public IActionResult GetBook(string title)
+    {
+        var book = _context.Books.Include(b => b.Author).FirstOrDefault(x => x.Title == title);
+        if (book == null)
+            return NotFound();
+        
+        return Ok(new Book
+        {
+            Title = book.Title,
+            AuthorUsername = book.Author.UserName
+        });
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> PostBook(Book book)
+    {
+        var author = await _context.Users.FirstOrDefaultAsync(x => x.UserName == book.AuthorUsername);
+        if (author == null)
+            return BadRequest("Author not found");
+        
+        _context.Books.Add(new Books {Title = book.Title, Author = author});
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetBook", new {title = book.Title}, book);
+    }
+}
---
+++ backend/React-AspNet-Identity/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace React_AspNet_Identity.Models;
+
+public class Book
+{
+    [Required] [JsonPropertyName("title")] 
+    public string Title { get; set; }
+    
+    [Required] [JsonPropertyName("author_username")]
+    public string AuthorUsername { get; set; }
+}
